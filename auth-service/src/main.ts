b7b0dd235949802a869d1cf0ed80af4aa270d6f9
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-03-06 23:51:18 +0300

    add bearer auth

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -29,6 +29,7 @@ async function bootstrap() {
     .setTitle('Minecraft server')
     .setDescription('Backend-service')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
