48274b286cb98575e9a091043429023f0a4c6c38
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-03-07 12:39:43 +0300

    fix log

--- auth-service/src/interceptors/Logging.ts
+++ auth-service/src/interceptors/Logging.ts
@@ -6,7 +6,7 @@ export class AppLoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { ip, method, path: url } = request;
+    const { ip, method, path: url, originalUrl } = request;
     const userAgent = request.get('user-agent') || '';
 
     response.on('close', () => {
@@ -14,7 +14,7 @@ export class AppLoggerMiddleware implements NestMiddleware {
       const contentLength = response.get('content-length');
 
       this.logger.log(
-        `${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`
+        `${method} ${url} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`
       );
     });
 
